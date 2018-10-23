755110334b5567d70d6dfb26aec028a07b914dc9
Eli Sennesh <e.sennesh@northeastern.edu>
2018-10-23 16:53:17 -0400

    src/Calculi: define a `check` function
    Signed-off-by: Eli Sennesh <e.sennesh@northeastern.edu>

--- src/Calculi.hs
+++ src/Calculi.hs
@@ -11,6 +11,18 @@ data ConstantExpr = IntConstant Int | BoolConstant Bool | StrConstant String
 data Expr = Var String | App Expr Expr | Abs String Expr |
             Constant ConstantExpr deriving (Eq, Show)
 
+check :: Expr -> Map.Map String ExprType -> Maybe ExprType
+check (App func arg) context = check func context >>= \case
+  FuncTy argType t -> Just t
+  _ -> Nothing
+  where
+    argType = check arg context
+check (Var name) context = Map.lookup name context
+check (Constant constant) _ = Just $ case constant of
+  IntConstant _ -> IntTy
+  BoolConstant _ -> BoolTy
+  StrConstant _ -> StringTy
+
 eval :: Expr -> Map.Map String Expr -> Maybe Expr
 eval (App func arg) context = eval func context >>= \case
   Abs name body -> eval body (Map.insert name arg context)
