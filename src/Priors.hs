1f60f16713a1208379c50b2de67789fca068fe9d
Eli Sennesh <sennesh.e@husky.neu.edu>
2018-11-26 15:56:00 -0500

    src/Priors: rewrite to use pattern functor
    Signed-off-by: Eli Sennesh <sennesh.e@husky.neu.edu>

--- src/Priors.hs
+++ src/Priors.hs
@@ -2,6 +2,7 @@ module Priors () where
 
 import Calculi
 import Control.Monad.Bayes.Class
+import Data.Functor.Foldable
 import qualified Data.Map as Map
 import Data.Maybe
 
@@ -38,15 +39,15 @@ string = do
     return [c]
 
 constExpr :: MonadSample m => Map.Map String ExprType -> ExprType -> m Expr
-constExpr _ IntTy = (Constant . IntConstant) <$> geometric 0.5
-constExpr _ BoolTy = (Constant . BoolConstant) <$> bernoulli 0.5
-constExpr _ StringTy = (Constant . StrConstant) <$> string
+constExpr _ IntTy = (Fix . Constant . IntConstant) <$> geometric 0.5
+constExpr _ BoolTy = (Fix . Constant . BoolConstant) <$> bernoulli 0.5
+constExpr _ StringTy = (Fix . Constant . StrConstant) <$> string
 
 operator :: MonadSample m => Map.Map String ExprType -> ExprType -> m Expr
 operator ctx (FuncTy a b) = do
   arg <- string
   body <- expr (Map.insert arg a ctx) b
-  return (Abs (arg, a) body)
+  return . Fix $ Abs (arg, a) body
 operator ctx t = do
   generate_constant <- bernoulli 0.5
   if generate_constant then
@@ -55,13 +56,13 @@ operator ctx t = do
     ta <- exprType
     a <- expr ctx ta
     func <- operator ctx (FuncTy ta t)
-    return (App func a)
+    return . Fix $ App func a
 
 var :: MonadSample m => Map.Map String ExprType -> ExprType -> Maybe (m Expr)
 var ctx t | compatibles /= Map.empty =
     Just (var_at_index <$> uniformD [0..length compatibles-1])
   where
-    var_at_index index = Var . fst $ (Map.toList compatibles !! index)
+    var_at_index index = Fix . Var . fst $ (Map.toList compatibles !! index)
     compatibles = Map.filter (== t) ctx
 var _ _ = Nothing
 
