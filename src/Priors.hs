baa6f82896589158608223f1ed919f02babb01c7
Eli Sennesh <esennesh@ccis.neu.edu>
2018-11-27 20:11:48 -0500

    src/Priors: include standard normal prior over doubles in constExpr
    Signed-off-by: Eli Sennesh <esennesh@ccis.neu.edu>

--- src/Priors.hs
+++ src/Priors.hs
@@ -43,6 +43,7 @@ constExpr :: MonadSample m => Map.Map String ExprType -> ExprType -> m Expr
 constExpr _ IntTy = (Fix . Constant . IntConstant) <$> geometric 0.5
 constExpr _ BoolTy = (Fix . Constant . BoolConstant) <$> bernoulli 0.5
 constExpr _ StringTy = (Fix . Constant . StrConstant) <$> string
+constExpr _ DoubleTy = (Fix . Constant . DoubleConstant) <$> normal 0.0 1.0
 
 operator :: MonadSample m => Map.Map String ExprType -> ExprType -> m Expr
 operator ctx (FuncTy a b) = do
