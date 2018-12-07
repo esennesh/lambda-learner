49612a0161587c0c485647f1c4b71c1dd9bc41f8
Eli Sennesh <esennesh@ccis.neu.edu>
2018-12-06 23:20:29 -0500

    Inference: generalize to arbitrary sampling monads and add particle param
    Signed-off-by: Eli Sennesh <esennesh@ccis.neu.edu>

--- src/Inference.hs
+++ src/Inference.hs
@@ -23,11 +23,20 @@ likelihood obsVal e = do
       return val
     Nothing -> return e
 
-importancePosterior :: MonadInfer m => Expr -> Weighted m Expr
-importancePosterior val =
+importancePosterior :: MonadSample m => Expr -> Weighted m Expr
+importancePosterior val | value val =
   scoreImportance (expand 0.1 val) priorScore >>= likelihood val where
     exprType = fromJust $ check val Map.empty
     priorScore = fromJust . exprScore Map.empty exprType
+importancePosterior _ = error "Observation is not a value"
 
-runWeightedPopulation :: MonadSample m => Weighted (Population m) a -> m [a]
-runWeightedPopulation = liftM (map fst) . runPopulation . resampleSystematic . applyWeight
+runWeightedPopulation :: MonadSample m => Population m a -> m [a]
+runWeightedPopulation = liftM (map fst) . runPopulation . resampleSystematic
+
+runProgramExpansion :: MonadSample m => Int -> Expr -> m [Expr]
+runProgramExpansion n val = runWeightedPopulation population where
+  population = spawn n >> posterior
+  posterior = fromWeightedList $ (:[]) <$> runWeighted (importancePosterior val)
+
+ioProgramExpansion :: Int -> Expr -> IO [Expr]
+ioProgramExpansion n val = sampleIO $ runProgramExpansion n val
