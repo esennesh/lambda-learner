9632d720f0b95a71e2b22fdc1527b1aa036e383a
Eli Sennesh <esennesh@ccis.neu.edu>
2018-12-08 17:49:34 -0500

    Rules: make sure to only match expandable expressions for expansion
    Signed-off-by: Eli Sennesh <esennesh@ccis.neu.edu>

--- src/Rules.hs
+++ src/Rules.hs
@@ -2,8 +2,10 @@ module Rules where
 
 import Calculi
 import Control.Applicative
+import Control.Exception
 import Control.Monad
 import Control.Monad.Bayes.Class
+import Control.Monad.Bayes.Sampler
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.Maybe
 import Data.Foldable
@@ -153,7 +155,7 @@ applyExpandLeft = pattern Pattern.var Pattern.var ->> \func arg -> do
   return (app func' arg)
   where
     pattern = mk2 $ \e -> case unfix e of
-      App f a -> Just (f, a)
+      App f a | expandable f -> Just (f, a)
       _ -> Nothing
 
 applyExpandRight :: MonadSample m => ExpansionRule m
@@ -162,7 +164,7 @@ applyExpandRight = pattern Pattern.var Pattern.var ->> \func arg -> do
   return (app func arg')
   where
     pattern = mk2 $ \e -> case unfix e of
-      App f a -> Just (f, a)
+      App f a | expandable a -> Just (f, a)
       _ -> Nothing
 
 flipExpandSample :: MonadSample m => ExpansionRule m
@@ -180,22 +182,27 @@ flipExpandProb = pattern Pattern.var ->> \prob -> do
   return (Calculi.flip prob')
   where
     pattern = mk1 $ \e -> case unfix e of
-      Flip prob -> Just prob
+      Flip prob | expandable prob -> Just prob
       _ -> Nothing
 
 expandRules :: MonadSample m => [ExpansionRule m]
 expandRules = [applyExpandAbs, applyExpandLeft, applyExpandRight,
                flipExpandSample, flipExpandProb]
 
+expandable :: Expr -> Bool
+expandable e = not $ null [r | r <- expandRules', isJust (tryMatch e r)] where
+  expandRules' = (expandRules :: [ExpansionRule SamplerIO])
+
+expandableValues :: Expr -> [Expr]
+expandableValues = filter expandable . values
+
 expandStep :: MonadSample m => Expr -> m Expr
-expandStep e = if numApplicable > 0 then do
+expandStep e = assert (numApplicable >= 1) $ do
     idx <- uniformD [0..numApplicable-1]
     e' <- match e (applicable !! idx)
     case check e' Map.empty of
       Just t' | t == t' -> return e'
       _ -> error $ "Expanded expression " ++ show e' ++ " has different type from original " ++ show e
-  else
-    return e
   where
     t = fromJust $ check e Map.empty
     applicable = [r | r <- expandRules, isJust (tryMatch e r)]
