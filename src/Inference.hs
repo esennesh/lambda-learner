72f4bdfff0557f0c2a0980b74a4f4d7748c72ee9
Eli Sennesh <esennesh@ccis.neu.edu>
2018-12-09 01:31:23 -0500

    Inference: rewrite inference to take many observations, randomize selection
    Randomize the selection of an observation to expand from among multiples, as
    well as sampling the bias towards shorter traces.
    
    Signed-off-by: Eli Sennesh <esennesh@ccis.neu.edu>

--- src/Inference.hs
+++ src/Inference.hs
@@ -23,25 +23,33 @@ likelihood obsVal e = do
       return val
     Nothing -> return e
 
-importancePosterior :: MonadSample m => Expr -> Weighted m Expr
-importancePosterior val | value val = do
-  e <- scoreImportance (expand 0.1 val) priorScore
-  likelihood val e
+importancePosterior :: MonadSample m => [Expr] -> Weighted m Expr
+importancePosterior vals | (all value vals) && (isJust exprTypeMaybe) = do
+  bias <- uniform 0.0 1.0
+  val <- uniformD vals
+  e <- scoreImportance (expand bias val) priorScore
+  foldr1 (>>) [likelihood val e | val <- vals]
   return e
   where
-    exprType = fromJust $ check val Map.empty
+    exprType = fromJust $ exprTypeMaybe
+    exprTypeMaybe = foldr1 eqAnd [check val Map.empty | val <- vals]
+    eqAnd :: Eq a => Maybe a -> Maybe a -> Maybe a
+    eqAnd (Just x) (Just x') | x == x' = Just x
+    eqAnd _ _ = Nothing
     priorScore e = case exprScore Map.empty exprType e of
       Just s -> s
       Nothing -> error $ "Expression of type " ++ (show $ check e Map.empty) ++ " =?= " ++ show exprType ++ " has no score"
-importancePosterior _ = error "Observation is not a value"
+importancePosterior vals | any (not . value) vals =
+  error "Observation is not a value"
+importancePosterior vals = error "Incompatible observation types"
 
 runWeightedPopulation :: MonadSample m => Population m a -> m [a]
 runWeightedPopulation = liftM (map fst) . runPopulation . resampleSystematic
 
-runProgramExpansion :: MonadSample m => Int -> Expr -> m [Expr]
-runProgramExpansion n val = runWeightedPopulation population where
+runProgramExpansion :: MonadSample m => Int -> [Expr] -> m [Expr]
+runProgramExpansion n vals = runWeightedPopulation population where
   population = spawn n >> posterior
-  posterior = fromWeightedList $ (:[]) <$> runWeighted (importancePosterior val)
+  posterior = fromWeightedList $ (:[]) <$> runWeighted (importancePosterior vals)
 
-ioProgramExpansion :: Int -> Expr -> IO [Expr]
-ioProgramExpansion n val = sampleIO $ runProgramExpansion n val
+ioProgramExpansion :: Int -> [Expr] -> IO [Expr]
+ioProgramExpansion n vals = sampleIO $ runProgramExpansion n vals
